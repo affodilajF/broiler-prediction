6f1c8444b69adeb3f485115b4f7052d62c063eae
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2025-09-20 01:19:25 +0700

    chore: env in dashboard controller

--- App/Controllers/DashboardController.py
+++ App/Controllers/DashboardController.py
@@ -4,22 +4,21 @@ sys.path.append(os.getcwd())
 
 # import middleware
 from App.Middleware import AuthMiddleware
-
-from dotenv import load_dotenv
 from flask import session, render_template, redirect, url_for
 
 # load env file
-load_dotenv(override=True)
+from App.Helpers.env_loader import load_environment
+load_environment()
 
 def index():
     if AuthMiddleware.check_xsrf_token():
-        return render_template('Pages/Admin/index.html', base_url = os.getenv('server_ip_address'))
+        return render_template('Pages/Admin/index.html', base_url = os.getenv('SERVER_IP_ADDRESS'))
     
     return redirect(url_for('web.auth'))
 
 def index_2():
     if AuthMiddleware.check_xsrf_token():
-        return render_template('Pages/Admin/index_2.html', base_url = os.getenv('server_ip_address'))
+        return render_template('Pages/Admin/index_2.html', base_url = os.getenv('SERVER_IP_ADDRESS'))
     
     return redirect(url_for('web.auth'))
     
